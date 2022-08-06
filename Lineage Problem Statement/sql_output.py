ffc2572fd8894be2b9bbb1ddc73e9fb3d78eaade
bharatkabra <bharat.kabra33@gmail.com>
2022-08-06 23:36:22 +0530

    updates

--- Lineage Problem Statement/sql_output.py
+++ Lineage Problem Statement/sql_output.py
@@ -8,7 +8,8 @@ Under Process..
 
 # import libraries
 import sqlparse
-from sqlparse.tokens import DML
+from sqlparse.sql import IdentifierList, Identifier
+from sqlparse.tokens import Keyword, DML
 
 
 def find_columnName_and_tableAlias(sql : str) -> dict:
@@ -59,3 +60,103 @@ def find_columnName_and_tableAlias(sql : str) -> dict:
             found_select = True
 
     return columns_alias
+
+def is_subselect(parsed: sqlparse.sql.Statement) -> bool:
+    '''
+        Description:
+        -----------
+        Function to iterate and check for first SELECT statement 
+
+        Parameters:
+        ---------
+        parsed (sqlparse.sql.Statement) : parsed sql statement object
+
+        Returns:
+        True/False (bool) : Boolean value
+        -------
+
+    '''
+    if not parsed.is_group:
+        return False
+    for item in parsed.tokens:
+        if item.ttype is DML and item.value.upper() == 'SELECT':
+            return True
+    return False
+
+
+def extract_from_part(parsed: sqlparse.sql.Statement) -> object:
+    '''
+        Description:
+        -----------
+        Recursive function to check statements after FROM keyword 
+
+        Parameters:
+        ---------
+        parsed (sqlparse.sql.Statement) : parsed sql statement object
+
+    '''
+    from_seen = False
+    for item in parsed.tokens:
+        if from_seen:
+            if is_subselect(item):
+                yield from extract_from_part(item)
+            elif item.ttype is Keyword:
+                return
+            else:
+#                 print(type(item))
+                yield item
+        elif item.ttype is Keyword and item.value.upper() == 'FROM':
+            from_seen = True
+
+
+def extract_table_identifiers(token_stream : list) -> dict:
+    '''
+        Description:
+        -----------
+        extract tablenames and it's alias 
+
+        Parameters:
+        ---------
+        token_stream (list) : list of sql parsed objects
+
+        Returns:
+        -------
+
+        tables (dict): dictionary containing tablenames and it's alias as key, value pair
+
+    '''
+    tables = {}
+    for item in token_stream:
+
+        if isinstance(item, IdentifierList):
+            for identifier in item.get_identifiers():
+                tables[identifier.get_name()] = identifier.value.split(" ")[:-1][identifier.value.split(" ")[:-1].index('FROM')+1].strip('(').strip(')')
+                
+                yield tables                  
+
+
+def get_originating_tables(sql_script: str) -> str:
+    """
+    Description :
+    ----------- 
+    Function to extract the resultant table names and column names
+
+    Parameters:
+    ----------
+    sql_script (str): string containing sql statement
+
+    Returns:
+    -------
+    final_sql (str): desired output of table names and column names
+    
+
+    """
+    columns = find_columnName_and_tableAlias(sql_script)
+    stream = extract_from_part(sqlparse.parse(sql_script)[0])
+    tables = dict(list(extract_table_identifiers(stream))[0])
+    final_sql = "column => table\n"
+    
+    for key, value in columns.items():
+        final_sql += str(columns[key]) + " => " + str(tables[value]) + "\n"
+
+    return final_sql
